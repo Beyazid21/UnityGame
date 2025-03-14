ca5ccccd5491aa8007f2e8c3dc563cfc576874bc
Beyazid21 <128732502+Beyazid21@users.noreply.github.com>
2025-03-14 18:48:06 +0400

    v0.8 interfaces added to Collectobles

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
@@ -5,23 +5,10 @@ public class PlayerInteractionController : MonoBehaviour
 {
    private void  OnTriggerEnter(Collider other) 
    {
-    if(other.CompareTag(Consts.WheatTypes.Gold_Wheat))
-    {
-       other.gameObject?.GetComponent<GoldWheatCollectible>().Collect();
+      if(other.gameObject.TryGetComponent<ICollectible>(out var collectible))
+      {
 
-    }
-
-    if(other.CompareTag(Consts.WheatTypes.Holy_Wheat))
-    {
-       other.gameObject?.GetComponent<HolyWheatCollectible>().Collect();
-        
-    }
-
-   if(other.CompareTag(Consts.WheatTypes.Rotten_Wheat))
-    {
-        other.gameObject?.GetComponent<RottenWheatCollectible>().Collect();
-        
-    }
-
+           collectible.Collect();
+      }
    }
 }
